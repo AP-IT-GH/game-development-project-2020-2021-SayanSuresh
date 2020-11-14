f9f0db2975991545e13f6b8b1ceb70db5523d156
sayan <sureshsayan77@gmail.com>
2020-11-14 15:00:01 +0100

    AnimationManager added 2 classes

---
+++ game2020/game2020/Animation/AnimationFrame.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace game2020.Animation
+{
+    public class AnimationFrame
+    {
+        public Rectangle SourceRectangle { get; set; }
+
+        public AnimationFrame(Rectangle rectangle)
+        {
+            SourceRectangle = rectangle;
+        }
+    }
+}
