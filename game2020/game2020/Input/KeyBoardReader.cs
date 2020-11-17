e89b9a2e1857e8837c851dfb7f6e62852b9eef3b
sayan <sureshsayan77@gmail.com>
2020-11-17 14:58:25 +0100

    Jumping movement added

--- game2020/game2020/Input/KeyBoardReader.cs
+++ game2020/game2020/Input/KeyBoardReader.cs
@@ -14,13 +14,12 @@ namespace game2020.Input
             var direction = Vector2.Zero;
             KeyboardState state = Keyboard.GetState();
             if (state.IsKeyDown(Keys.Left))
-            {
                 direction = new Vector2(-1, 0);
-            }
             if (state.IsKeyDown(Keys.Right))
-            {
                 direction = new Vector2(1, 0);
-            }
+            if (state.IsKeyDown(Keys.Up))
+                direction = new Vector2(direction.X, -1);
+
             return direction;
         }
     }
--- game2020/game2020/Players/Hero.cs
+++ game2020/game2020/Players/Hero.cs
@@ -14,9 +14,14 @@ namespace game2020.Players
     {
         private Rectangle _collisionRectangle;
         public Rectangle CollisionRectangle { get; set; }
-        public Vector2 Position { get ; set; }
+        public Vector2 Position { get; set; }
 
         Texture2D heroTexture;
+        GameTime gameTime;
+        Vector2 velocity;
+        private bool jump = false;
+        const float gravity = 100f;
+        float jumpspeed = 1000;
 
         private IInputReader reader;
         private IGameCommand moveCommand;
@@ -35,10 +40,8 @@ namespace game2020.Players
             this.reader = inputReader;
             moveCommand = new MoveCommand();
 
-            Position = new Vector2(0, 0);
 
             _collisionRectangle = new Rectangle((int)Position.X, (int)Position.Y, 48, 62);
-
         }
 
         private void move(Vector2 _direction)
@@ -47,6 +50,32 @@ namespace game2020.Players
                 currentAnimation = walkLeft;
             else if (_direction.X == 1)
                 currentAnimation = walkRight;
+
+            // jumping movement
+            if (_direction.Y == -1 && jump)
+            {
+                velocity.Y = -jumpspeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+                jump = false;
+            }
+
+            if (!jump)
+                velocity.Y += gravity * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            else
+                velocity.Y = 0;
+
+            Position += velocity;
+
+            jump = Position.Y >= 300;
+
+            if (jump)
+            {
+                Position = new Vector2(Position.X, 300);
+                if (_direction.X == -1)
+                    Position = new Vector2(Position.X, 300);
+                else if (_direction.X == 1)
+                    Position = new Vector2(Position.X, 300);
+            }
+
             moveCommand.Execute(this, _direction);
         }
 
@@ -58,13 +87,11 @@ namespace game2020.Players
         public void Update(GameTime gameTime)
         {
             var direction = reader.ReadInput();
-
+            this.gameTime = gameTime;
             if (direction.X != 0 || direction.Y != 0)
-            {
                 //animatie.Update(gameTime);
-                currentAnimation.Update(gameTime);
-                move(direction);
-            }
+                currentAnimation.Update(this.gameTime);
+            move(direction);
 
             _collisionRectangle.X = (int)Position.X;
             CollisionRectangle = _collisionRectangle;
