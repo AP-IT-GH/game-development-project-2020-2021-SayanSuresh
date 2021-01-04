78625bd22a0ab6352749670309d70f80c52814f3
sayan <sureshsayan77@gmail.com>
2021-01-04 22:57:01 +0100

    testing

--- game2020/game2020/Collision/CollisionWithEnemy.cs
+++ game2020/game2020/Collision/CollisionWithEnemy.cs
@@ -9,14 +9,10 @@ namespace game2020.Collision
 {
     public class CollisionWithEnemy 
     {
-        public int i { get; set; } = 0;
         public void HandleHeroSpawn(ICheckCollision collision, ITransform heroTransform)
         {
             if (collision.CollisionWithBot)
                 heroTransform.Position = new Vector2(200, 30);
-            //Debug.WriteLine($"testi{i++}");
-            //else
-            //heroTransform.Position = new Vector2(30, 30);
         }
     }
 }
